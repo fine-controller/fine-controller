18412312adecc268ad41d1c0be7657ffeb014c50
Fortune Ngwenya <fortune.ngwenya@platform45.com>
2023-04-28 14:26:04 +0200

    update

--- push.ps1
+++ push.ps1
@@ -1,13 +1,4 @@
-﻿# GitHub
-
-git add .
+﻿git add .
 git stage .
 git commit -am update
 git push
-
-# DockerHub
-
-docker build . -t fortunen/fine-kube-operator
-docker tag fortunen/fine-kube-operator fortunen/fine-kube-operator
-docker push fortunen/fine-kube-operator
-docker pushrm fortunen/fine-kube-operator # https://github.com/christian-korneck/docker-pushrm
